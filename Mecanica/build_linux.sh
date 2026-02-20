d7c33bbaa9d29d8dec861bf02f8b06b5ade6a3e6
hutanupauliustin <hutanupauliustin@gmail.com>
2026-02-20 12:43:38 +0200

    adsa

--- Mecanica/build_linux.sh
+++ Mecanica/build_linux.sh
@@ -4,10 +4,8 @@ clear
 
 echo -e "\e[36m==> Începem compilarea pe Linux...\e[0m"
 
-# Am pus -B/usr/bin/ ca să forțăm uneltele sistemului și -lglfw standard
-g++ src/main.cpp src/glad.c -o simulator -I includes -B/usr/bin/ -lglfw -lGL -lm -ldl
+g++ src/*.cpp src/glad.c -o simulator -I includes -B/usr/bin/ -L/usr/lib/x86_64-linux-gnu -lglfw -lGL -lm -ldl
 
-# 3. Verificăm dacă a compilat cu succes
 if [ $? -eq 0 ]; then
     echo -e "\e[32m==> Compilare reușită! 😎 \e[0m"
     ./simulator
