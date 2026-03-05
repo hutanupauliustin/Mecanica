15ea3a14b12d30dc30f339793d28ade22c0473ab
hutanupauliustin <hutanupauliustin@gmail.com>
2026-03-05 19:02:03 +0200

    am reparat executabilul

--- Mecanica/build_windows.ps1
+++ Mecanica/build_windows.ps1
@@ -5,9 +5,11 @@ Write-Host "==> Începem compilarea..." -ForegroundColor Cyan
 # Gaseste automat toate fisierele .cpp si .c din directorul src
 $sourceFiles = Get-ChildItem -Path src -Recurse -Include *.cpp, *.c | ForEach-Object { $_.FullName }
 
+
 Write-Host "Compilam fisierele: $sourceFiles"
 
-g++ $sourceFiles -o simulator.exe -I includes -L lib -lglfw3 -lgdi32 -lopengl32
+g++ $sourceFiles -o simulator.exe -I includes -L lib -lglfw3 -lgdi32 -lopengl32 -static -static-libgcc -static-libstdc++
+
 
 if ($?) {
     Write-Host "==> Compilare reusita!" -ForegroundColor Green
